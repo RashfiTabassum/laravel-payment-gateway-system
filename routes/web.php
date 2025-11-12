f8bcaec2035906eebe2734011e84f5b7b9533710
Rashme22 <aktherrashme22@gmail.com>
2025-11-12 08:40:30 +0600

    Resolve merge conflict and add BankController routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
-
+use App\Http\Controllers\BankController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +15,7 @@ use App\Http\Controllers\DashboardController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', fn() => redirect()->route('login'));
+Route::get('/', fn () => redirect()->route('login'));
 
 Route::middleware('guest')->group(function () {
     Route::get('login',    [AuthController::class, 'showLogin'])->name('login');
@@ -24,9 +25,13 @@ Route::middleware('guest')->group(function () {
     Route::post('register',[AuthController::class, 'register'])->name('register.post');
 });
 
-
-
 Route::middleware('auth')->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+   
+    Route::resource('admins', AdminController::class)
+        ->names('admins')
+        ->except(['show']);
+
+
 });
