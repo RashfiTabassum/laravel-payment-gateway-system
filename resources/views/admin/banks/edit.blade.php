0d1f5afb53a527aa006582c87027642e4b18dc13
Riyadh <monjur.morshed@softrobotics.com.bd>
2025-11-12 19:36:40 +0600

    initial commit

--- resources/views/admin/banks/edit.blade.php
+++ resources/views/admin/banks/edit.blade.php
@@ -3,26 +3,39 @@
 @section('title', 'Edit Bank')
 
 @section('content')
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title">Edit Bank</h3>
-    </div>
+    {{-- Global alert messages --}}
+    @include('admin.partials.alerts')
 
-    <div class="card-body">
-        <form method="POST" action="{{ route('admin.banks.update', $bank->id) }}">
-            @csrf
-            @method('PUT')
+    <div class="app-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card mt-3">
+                        <div class="card-header">
+                            <div class="row mb-3">
+                                <div class="col-md-12 d-flex justify-content-between align-items-center">
+                                    <h3>Edit Bank</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('admin.banks.update', $bank->id) }}">
+                                @csrf
+                                @method('PUT')
 
-            @include('admin.banks.form')
+                                @include('admin.banks.form')
 
-            <div class="mt-3">
-                <button type="submit" class="btn btn-primary">Update</button>
-                <a href="{{ route('admin.banks.index') }}" class="btn btn-secondary">Cancel</a>
+                                <div class="mt-3">
+                                    <button type="submit" class="btn btn-primary">Update</button>
+                                    <a href="{{ route('admin.banks.index') }}" class="btn btn-secondary">Cancel</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </form>
+        </div>
     </div>
-</div>
 @endsection
 
 
- 
